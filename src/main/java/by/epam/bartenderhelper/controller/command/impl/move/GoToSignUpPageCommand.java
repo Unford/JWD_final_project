00cd90caafdfab961bdaf2b46a5bdd12ec18fb46
vladimir_feshenko <vladimir0feshenko@gmail.com>
2022-01-31 16:52:20 +0300

    prg pattern login

---
+++ src/main/java/by/epam/bartenderhelper/controller/command/impl/move/GoToSignUpPageCommand.java
@@ -0,0 +1,13 @@
+package by.epam.bartenderhelper.controller.command.impl.move;
+
+import by.epam.bartenderhelper.controller.command.Command;
+import by.epam.bartenderhelper.controller.command.Router;
+import by.epam.bartenderhelper.exception.CommandException;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class GoToSignUpPageCommand implements Command {
+    @Override
+    public Router execute(HttpServletRequest request) throws CommandException {
+        return null;
+    }
+}
